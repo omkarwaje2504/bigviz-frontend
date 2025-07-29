77ef18572848e9fc3384cd3844e2843f2e45e18f
vedant <vedantbhadkamkar123@gmail.com>
2025-07-29 14:18:04 +0530

    props update

--- src/components/remotion-components/Sequence.jsx
+++
@@ -1,9 +0,0 @@
-const Sequence = ({ frame, from, durationInFrames, children }) => {
-  if (frame >= from && frame <= durationInFrames) {
-    return <div>{children}</div>;
-  } else {
-    null;
-  }
-};
-
-export default Sequence;
