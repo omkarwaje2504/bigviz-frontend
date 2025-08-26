a38add2fe24cb51cc11b4b4e23188c9d355e2d74
omkarwaje2504 <omkar@saiashirwad.com>
2025-08-26 12:06:34 +0530

    return back to webpack

--- next.config.js
+++ next.config.js
@@ -1,51 +1,68 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: { resolveAlias: { canvas: "./empty-module.js" } },
-  trailingSlash: true,
   async headers() {
     return [
       {
         source: "/",
         headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
           {
             key: "Cache-Control",
             value: "no-store, no-cache, must-revalidate, proxy-revalidate",
           },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
         ],
       },
     ];
   },
   productionBrowserSourceMaps: true,
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.devtool = "source-map";
-  //   }
-  //   config.resolve.alias["canvas"] = false;
-  //   return config;
-  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+    config.resolve.alias["canvas"] = false;
+    return config;
+  },
   reactStrictMode: false,
   output: "export",
   distDir: "dist",
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: "https", hostname: "img.freepik.com" },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
       {
         protocol: "https",
         hostname: "pub-0b6394cfeda24bf196c98e1746afe09b.r2.dev",
       },
-      { protocol: "https", hostname: "pixpro.s3.ap-south-1.amazonaws.com" },
+      {
+        protocol: "https",
+        hostname: "pixpro.s3.ap-south-1.amazonaws.com",
+      },
       {
         protocol: "https",
         hostname: "pixpro-video-generation.s3.ap-south-1.amazonaws.com",
       },
-      { protocol: "https", hostname: "bigviz-frontend.projectcampaign.online" },
+      {
+        protocol: "https",
+        hostname: "bigviz-frontend.projectcampaign.online",
+      },
     ],
   },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
